47f67a1c7c0a9a17a93725f260321901d93fc62a
Yony <arleybri18@gmail.com>
2019-09-28 16:13:58 -0500

    [feature] implement return one eps by id

--- api/v1/views/eps.py
+++ api/v1/views/eps.py
@@ -22,3 +22,23 @@ def views_eps():
         list_eps.append(dict_eps)
         dict_eps = {}
     return jsonify(list_eps)
+
+
+@app_views.route('/eps/<eps_id>')
+def views_eps_id(eps_id=None):
+    """
+    Return the current eps in a JSON file form
+    """
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM eps where id= {}'.format(eps_id))
+    eps = cur.fetchall()
+    list_eps = []
+    dict_eps = {}
+    for el in eps:
+        dict_eps.update({"id": el[0]})
+        dict_eps.update({"name": el[1]})
+        dict_eps.update({"phone": el[2]})
+        dict_eps.update({"free_line": el[3]})
+        list_eps.append(dict_eps)
+        dict_eps = {}
+    return jsonify(list_eps)
